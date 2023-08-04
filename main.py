71a2ea97115a301b6f6d4d438702172c47e0dae9
Wenliang WANG <wwangbl@connect.ust.hk>
2023-08-04 14:33:59 +0800

    updates

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import pdfkit
 from jinja2 import Environment, FileSystemLoader
+import pandas as pd
+import os
+import webbrowser
 
-def create_pdf(data, template_path="templates/report.html", output="report.pdf"):
+def create_pdf(data, template_path="report.html", output="report.pdf"):
     # Load your Jinja2 template
-    env = Environment(loader=FileSystemLoader('.'))
+    env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template(template_path)
 
     # Render the template with your data
     rendered_html = template.render(data=data)
 
     # Create the pdf
-    pdfkit.from_string(rendered_html, output, options={'enable-local-file-access': True})
+    pdfkit.from_string(rendered_html, output, options={'enable-local-file-access': ''})
+
+    # Open the pdf
+    webbrowser.open_new_tab(output)
+
+# Read the CSV data
+df = pd.read_csv('data/table.csv')
+
+# Convert the DataFrame into a list of dictionaries
+table_data = df.to_dict('records')
 
 # Data from your .csv, .json, .txt, etc.
 data = {
     "name": "John Doe",
     "age": 25,
-    # Add more data here...
+    "logo_path": os.path.abspath("templates/test.png"),
+    "table": table_data
 }
 
 create_pdf(data)
