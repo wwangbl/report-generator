b83d0f01668d105c721f12e8b34695f186e33320
Wenliang WANG <wwangbl@connect.ust.hk>
2023-08-07 18:04:30 +0800

    updates

--- main.py
+++ main.py
@@ -20,16 +20,31 @@ def create_pdf(data, template_path="report.html", output="report.pdf"):
 
 # Read the CSV data
 df = pd.read_csv('data/table.csv')
+tb_summary = pd.read_csv('data/summary.csv')
+tb_variants = pd.read_csv('data/variants.csv')
+tb_variant_details = pd.read_csv('data/variant_details.csv')
+tb_drug_summary = pd.read_csv('data/drug_summary.csv')
 
 # Convert the DataFrame into a list of dictionaries
 table_data = df.to_dict('records')
+table_summary = tb_summary.to_dict('records')
+table_variants = tb_variants.to_dict('records')
+table_variant_details = tb_variant_details.to_dict('records')
+table_drug_summary = tb_drug_summary.to_dict('records')
 
 # Ask for user input, with default values
-name = input("Enter name: ") or "John Doe"
-id = input("Enter ID: ") or "A123456"
-gender = input("Enter gender: ") or "Male"
-age = input("Enter age: ") or 25
-dob = input("Enter date-of-birth: ") or "01/11/1990"
+# name = input("Enter name: ") or "John Doe"
+# id = input("Enter ID: ") or "A123456"
+# gender = input("Enter gender: ") or "Male"
+# age = input("Enter age: ") or 25
+# dob = input("Enter date-of-birth: ") or "01/11/1990"
+
+name = "John Doe"
+id = "A123456"
+gender = "Male"
+age = 25
+dob = "01/11/1990"
+
 logo_path = "templates/test.png"
 
 # Data from your .csv, .json, .txt, etc.
@@ -40,7 +55,11 @@ data = {
     "gender": gender,
     "dob": dob,
     "logo_path": os.path.abspath(logo_path),
-    "table": table_data
+    "table": table_data,
+    "table_summary": table_summary,
+    "table_variants": table_variants,
+    "table_variant_details": table_variant_details,
+    "table_drug_summary": table_drug_summary,
 }
 
 create_pdf(data)
